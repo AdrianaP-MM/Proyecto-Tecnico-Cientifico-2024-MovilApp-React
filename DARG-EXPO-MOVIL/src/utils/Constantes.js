f3d3f16ecb97bf7d13f12fc9102f2537052aea51
AdrianaP-MM <20190015@ricaldone.edu.sv>
2024-08-06 17:18:29 -0600

    Actualizacion del uso de constantes

--- DARG-EXPO-MOVIL/src/utils/Constantes.js
+++ DARG-EXPO-MOVIL/src/utils/Constantes.js
@@ -1,12 +1,10 @@
-//IP de donde se encuentra la Api, en este caso la dirección 
-//Ip de la maquina locala 
-//utilizar la direccion IP del servidor y no localhost
-export const Config = {
-    IP: 'http://192.168.2.113',
-    SERVER_URL:`${Config.IP}/PROYECTO-TECNICO-CIENTIFICO-2024/api/services/publico/`,
-    IMAGE_URL: `${Config.IP}/PROYECTO-TECNICO-CIENTIFICO-2024/api/images/`
-};
-
-
-
-
+// IP de donde se encuentra la Api, en este caso la dirección 
+// IP de la máquina local
+// utilizar la dirección IP del servidor y no localhost
+const createConfig = (ip) => ({
+    IP: ip,
+    SERVER_URL: `${ip}/PROYECTO-TECNICO-CIENTIFICO-2024/api/services/publico/`,
+    IMAGE_URL: `${ip}/PROYECTO-TECNICO-CIENTIFICO-2024/api/images/`
+});
+const Config = createConfig('http://192.168.2.113'); // Aquí cambian la IP
+export default Config;
