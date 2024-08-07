bf31ac9f3bd709369a895d07e8035559fa9b4d07
AlissonZepeda07 <20220127@ricaldone.edu.sv>
2024-08-07 12:10:08 -0600

    diseño de modals para cambiar contraseña en login listos
    estos apareceran cuando una contraseña ingresada sea 00000000

--- DARG-EXPO-MOVIL/src/screens/login.js
+++ DARG-EXPO-MOVIL/src/screens/login.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { View, StyleSheet, Alert } from 'react-native';
-import { Avatar, TouchableRipple } from 'react-native-paper';
+import { Avatar, Provider, TouchableRipple, Dialog, Portal } from 'react-native-paper';
 import Text from '../components/utilidades/Text'; // Importa el componente de texto personalizado
 import Button from '../components/buttons/ButtonRojo'; // Importa el botón personalizado
 import Input from '../components/inputs/AllBorder'; // Importa el componente de entrada personalizado
@@ -11,7 +11,15 @@ export default function Login({ navigation }) {
   // Define los estados para el correo electrónico y la contraseña
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
+  const [codigo, setCodigo] = React.useState('');
+  const [passwordReset, setpasswordReset] = React.useState('');
   const API = 'usuarios_clientes.php';
+
+
+  const [visiblePersonaDialog, setVisiblePersonaDialog] = React.useState(true);
+  const [visibleCamposDialog, setVisibleCamposDialog] = React.useState(false);
+  const [visibleCamposCodigo, setvisibleCamposCodigo] = React.useState(false);
+  const [visibleCamposContraseña, setvisibleCamposContraseña] = React.useState(false);
 
   const handleLogin = async () => {
 
@@ -57,51 +65,157 @@ export default function Login({ navigation }) {
     }
   };
 
+  // Handle the next button of the first dialog
+  const handleNext = () => {
+    setVisiblePersonaDialog(false);
+    if (checked === 'Persona juridica') {
+      setVisibleCamposDialog(true);
+    }
+  };
+
+
+  const handleAbrirDialogo = () => {
+    // Si todos los campos están llenos, cerrar el diálogo y proceder
+    setVisibleCamposDialog(true);
+  };
+
+  const handleAbrirCodigo = () => {
+    // Si todos los campos están llenos, cerrar el diálogo y proceder
+    setVisibleCamposDialog(false);
+    setvisibleCamposCodigo(true);
+  };
+
+  const handleAbrirCambiarContraseña = () => {
+    // Si todos los campos están llenos, cerrar el diálogo y proceder
+    setvisibleCamposCodigo(false);
+    setvisibleCamposContraseña(true);
+  };
+
+  const handlePasswordRessetExitoso = () => {
+    // Si todos los campos están llenos, cerrar el diálogo y proceder
+    setvisibleCamposContraseña(false);
+    Alert.alert('Exito', 'Ahora puede iniciar sesion');
+  };
+
   return (
-    <View style={styles.container}>
-      <StatusBar style="dark" backgroundColor="#ffffff" />
-      <View style={styles.containerTotal}>
-        <View style={styles.avatarContainer}>
-          <Avatar.Icon size={75} icon="alpha-d" style={styles.avatarIcon} />
-          <Text texto='Inicio de sesión' font='PoppinsBold' fontSize={20} />
-          <Text texto='Bienvenido a DARG' font='PoppinsMedium' fontSize={14} />
-        </View>
-        <Input
-          placeholder='Correo'
-          value={email}
-          onChangeText={setEmail}
-          width='95%'
-          iconImage={(require('../images/icons/iconUser.png'))}
-        />
-        <Input
-          placeholder='Contraseña'
-          value={password}
-          onChangeText={setPassword}
-          width='95%'
-          iconImage={(require('../images/icons/iconLock.png'))}
-          secureTextEntry={true}
-        />
-        <View>
-          <TouchableRipple
-            onPress={() => navigation.navigate('RestablecerContra')}
-            rippleColor="rgba(0, 0, 0, .32)"
-          >
-            <Text texto='¿Olvidaste tu contraseña?' font='PoppinsMedium' fontSize={14} textAlign='center' />
-          </TouchableRipple>
-        </View>
-        <Button textoBoton='Iniciar sesión' accionBoton={handleLogin} fontSize={17} width='90%' marginTop={50} />
-        <Button textoBoton='cerrar wea' accionBoton={handleCerrarSesion} fontSize={17} width='90%' marginTop={50} />
-        <View style={styles.registerContainer}>
-          <Text texto='¿No tienes cuenta? ' font='PoppinsRegular' fontSize={14} textAlign='center' />
-          <TouchableRipple
-            onPress={() => navigation.navigate('Registrate')}
-            rippleColor="rgba(0, 0, 0, .32)"
-          >
-            <Text texto='Registrate' font='PoppinsSemiBold' fontSize={15} textAlign='center' color='red' />
-          </TouchableRipple>
+
+    <Provider>
+
+      <Portal>
+        <Dialog //Dialogo para pedir el correo del cliente y asi enviar el codigo y luego cambiar la contraseña
+          visible={visibleCamposDialog}
+          onDismiss={() => setVisibleCamposDialog(false)}
+          style={styles.dialog}
+        >
+          <Dialog.Title style={styles.dialogTitle}>
+            <Text texto='Contraseña no segura, ayudanos a cambiarla' font='PoppinsSemiBold' fontSize={20} textAlign='center' color='#3B3939' />
+          </Dialog.Title>
+          <Dialog.Content>
+            <Input
+              placeholder='Correo'
+              value={email}
+              onChangeText={setEmail}
+              width='95%'
+              iconImage={(require('../images/icons/iconUser.png'))}
+            />
+          </Dialog.Content>
+          <Dialog.Actions style={styles.center}>
+            <Button textoBoton='Siguiente' accionBoton={handleAbrirCodigo} fontSize={15} width='55%' />
+          </Dialog.Actions>
+        </Dialog>
+
+        <Dialog //Dialogo para ingresar el codigo proporcionado al correo
+          visible={visibleCamposCodigo}
+          onDismiss={() => setvisibleCamposCodigo(false)}
+          style={styles.dialog}
+        >
+          <Dialog.Title style={styles.dialogTitle}>
+            <Text texto='Ingresa el codigo que te enviamos al correo' font='PoppinsSemiBold' fontSize={20} textAlign='center' color='#3B3939' />
+          </Dialog.Title>
+          <Dialog.Content>
+            <Input
+              placeholder='Codigo'
+              value={codigo}
+              onChangeText={setCodigo}
+              width='95%'
+              iconImage={(require('../images/icons/iconContra.png'))}
+            />
+          </Dialog.Content>
+          <Dialog.Actions style={styles.center}>
+            <Button textoBoton='Siguiente' accionBoton={handleAbrirCambiarContraseña} fontSize={15} width='55%' />
+          </Dialog.Actions>
+        </Dialog>
+
+        <Dialog //Dialogo para cambiar contraseña
+          visible={visibleCamposContraseña}
+          onDismiss={() => setvisibleCamposContraseña(false)}
+          style={styles.dialog}
+        >
+          <Dialog.Title style={styles.dialogTitle}>
+            <Text texto='Ingresa una nueva contraseña' font='PoppinsSemiBold' fontSize={20} textAlign='center' color='#3B3939' />
+          </Dialog.Title>
+          <Dialog.Content>
+            <Input
+              placeholder='Nueva contraseña'
+              value={passwordReset}
+              onChangeText={setpasswordReset}
+              width='95%'
+              iconImage={(require('../images/icons/iconContra.png'))}
+            />
+          </Dialog.Content>
+          <Dialog.Actions style={styles.center}>
+            <Button textoBoton='Siguiente' accionBoton={handlePasswordRessetExitoso} fontSize={15} width='55%' />
+          </Dialog.Actions>
+        </Dialog>
+
+      </Portal>
+
+      <View style={styles.container}>
+        <StatusBar style="dark" backgroundColor="#ffffff" />
+        <View style={styles.containerTotal}>
+          <View style={styles.avatarContainer}>
+            <Avatar.Icon size={75} icon="alpha-d" style={styles.avatarIcon} />
+            <Text texto='Inicio de sesión' font='PoppinsBold' fontSize={20} />
+            <Text texto='Bienvenido a DARG' font='PoppinsMedium' fontSize={14} />
+          </View>
+          <Input
+            placeholder='Correo'
+            value={email}
+            onChangeText={setEmail}
+            width='95%'
+            iconImage={(require('../images/icons/iconUser.png'))}
+          />
+          <Input
+            placeholder='Contraseña'
+            value={password}
+            onChangeText={setPassword}
+            width='95%'
+            iconImage={(require('../images/icons/iconLock.png'))}
+            secureTextEntry={true}
+          />
+          <View>
+            <TouchableRipple
+              onPress={() => navigation.navigate('RestablecerContra')}
+              rippleColor="rgba(0, 0, 0, .32)"
+            >
+              <Text texto='¿Olvidaste tu contraseña?' font='PoppinsMedium' fontSize={14} textAlign='center' />
+            </TouchableRipple>
+          </View>
+          <Button textoBoton='Iniciar sesión' accionBoton={handleLogin} fontSize={17} width='90%' marginTop={50} />
+          <Button textoBoton='cerrar wea' accionBoton={handleCerrarSesion} fontSize={17} width='90%' marginTop={50} />
+          <Button textoBoton='Abrir dialogo' accionBoton={handleAbrirDialogo} fontSize={17} width='90%' marginTop={50} />
+          <View style={styles.registerContainer}>
+            <Text texto='¿No tienes cuenta? ' font='PoppinsRegular' fontSize={14} textAlign='center' />
+            <TouchableRipple
+              onPress={() => navigation.navigate('Registrate')}
+              rippleColor="rgba(0, 0, 0, .32)"
+            >
+              <Text texto='Registrate' font='PoppinsSemiBold' fontSize={15} textAlign='center' color='red' />
+            </TouchableRipple>
+          </View>
         </View>
       </View>
-    </View>
+    </Provider>
   );
 }
 
@@ -125,11 +239,16 @@ const styles = StyleSheet.create({
   },
   avatarIcon: {
     marginBottom: 10, // Espacio debajo del icono del avatar
+    backgroundColor: '#BA181B',
   },
   registerContainer: {
     flexDirection: 'row', // Organiza los elementos en fila
     justifyContent: 'center', // Centra el contenido horizontalmente
     alignItems: 'center', // Centra el contenido verticalmente
     marginTop: 20, // Espacio encima del contenedor de registro
+  },
+  dialog: {
+    borderRadius: 10,
+    backgroundColor: 'white'
   },
 });
