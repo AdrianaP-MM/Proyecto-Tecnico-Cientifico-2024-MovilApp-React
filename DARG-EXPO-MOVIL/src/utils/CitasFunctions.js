b2f3c7158aeb7bf13269641ed2893b5139f46b6a
AdrianaP-MM <20190015@ricaldone.edu.sv>
2024-09-17 21:08:12 -0600

    Arreglo en citasFunciones

--- DARG-EXPO-MOVIL/src/utils/CitasFunctions.js
+++ DARG-EXPO-MOVIL/src/utils/CitasFunctions.js
@@ -1,13 +1,13 @@
-export const verDetalles = async (navigation, id_cita, fecha, hora, auto, movilizacion, zona, ida, regreso, estado) => {
+export const verDetalles = (navigation, cita) => {
     navigation.navigate('Detalles de la cita', {
-        id_cita: id_cita,
-        fecha: fecha,
-        hora: hora,
-        auto: auto,
-        movilizacion: movilizacion,
-        zona: zona,
-        ida: ida,
-        regreso: regreso,
-        estado: estado
+        id_cita: cita.id_cita,
+        fecha: cita.fecha_cita,
+        hora: cita.hora_cita,
+        auto: cita.id_automovil,
+        movilizacion: cita.movilizacion_vehiculo,
+        zona: cita.zona_habilitada, 
+        ida: cita.direccion_ida, 
+        regreso: cita.direccion_regreso, 
+        estado: cita.estado 
     });
 };
