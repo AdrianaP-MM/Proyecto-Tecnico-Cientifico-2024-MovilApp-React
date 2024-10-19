3a569c595e5f948cb4a5b14f0c28a6d3d1282835
AxelGarciaR <20220127@ricaldone.edu.sv>
2024-10-18 22:19:26 -0600

    Cambios en la pagina de agregar carros

--- DARG-EXPO-MOVIL/src/screens/AgregarCarro.js
+++ DARG-EXPO-MOVIL/src/screens/AgregarCarro.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Image, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import { ScrollView, View, Text, Image, TouchableOpacity, StyleSheet, Alert } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import Button from '../components/buttons/ButtonRojo'; // Importa el componente de botón personalizado
 import Input from '../components/inputs/AllBorder'; // Importa el componente de entrada personalizado
@@ -37,10 +37,16 @@ const AgregarVehiculo = ({ navigation }) => {
     try {
       const responseTiposAutomoviles = await fetchData(API, 'readTipos');
       if (responseTiposAutomoviles.status) {
-        setPickerValuesTipos(responseTiposAutomoviles.dataset.map(item => ({
-          label: item.nombre_tipo_automovil, // Asegúrate de que el campo nombre sea correcto
-          value: item.id_tipo_automovil, // Asegúrate de que el campo id sea correcto
-        })));
+        // Agregar un elemento predeterminado
+        const tiposConDefault = [{
+          label: 'Selecciona un tipo', // Label predeterminado
+          value: null, // Valor que no se utilizará
+        }, ...responseTiposAutomoviles.dataset.map(item => ({
+          label: item.nombre_tipo_automovil,
+          value: item.id_tipo_automovil,
+        }))];
+
+        setPickerValuesTipos(tiposConDefault);
       } else {
         Alert.alert('Error', `${responseTiposAutomoviles.error}` + '. Es necesario registrar un automóvil antes de agendar una cita.');
       }
@@ -54,10 +60,16 @@ const AgregarVehiculo = ({ navigation }) => {
     try {
       const responseMarcasAutomoviles = await fetchData(API, 'readMarcas');
       if (responseMarcasAutomoviles.status) {
-        setPickerValuesMarca(responseMarcasAutomoviles.dataset.map(item => ({
-          label: item.nombre_marca_automovil, // Asegúrate de que el campo nombre sea correcto
-          value: item.id_marca_automovil, // Asegúrate de que el campo id sea correcto
-        })));
+        // Agregar un elemento predeterminado
+        const marcasConDefault = [{
+          label: 'Selecciona una marca', // Label predeterminado
+          value: null, // Valor que no se utilizará
+        }, ...responseMarcasAutomoviles.dataset.map(item => ({
+          label: item.nombre_marca_automovil,
+          value: item.id_marca_automovil,
+        }))];
+
+        setPickerValuesMarca(marcasConDefault);
       } else {
         Alert.alert('Error', `${responseMarcasAutomoviles.error}` + '. Es necesario registrar un automóvil antes de agendar una cita.');
       }
@@ -92,8 +104,35 @@ const AgregarVehiculo = ({ navigation }) => {
     }
   };
 
-  // Función para manejar la acción de guardar el carro
+  function validateSalvadoranPlate(plate) {
+    // Expresión regular para validar el formato de la placa salvadoreña
+    const plateRegex = /^(A|AB|C|CC|CD|D|E|F|M|MB|MI|N|O|P|PR|PNC|RE|T|V)-?[A-Za-z0-9]{3}-[A-Za-z0-9]{3}$/;
+
+    // Validar formato
+    if (!plateRegex.test(plate)) {
+      return { valid: false, message: 'Formatos de placa salvadoreños: Un subfijo: P-###-###, dos subfijos: AB-###-###, tres subfijos PNC-###-###. Verfica tambien la informacion de placa.' };
+    }
+
+    return { valid: true, message: 'Placa válida.' };
+  }
+
   const handleGuardarCarro = async () => {
+    // Validar la placa antes de proceder
+    const validationResult = validateSalvadoranPlate(placa);
+    if (!validationResult.valid) {
+      Alert.alert('Error', validationResult.message);
+      return; // Detener la ejecución si la placa no es válida
+    }
+
+    // Validar que la fecha no sea mayor que la fecha actual
+    const fechaActual = new Date();
+    const fechaFabricacion = new Date(fecha); // Asegúrate de que `fecha` tenga un formato válido
+    if (fechaFabricacion > fechaActual) {
+      Alert.alert('Error', 'La fecha de fabricación no puede ser mayor a la fecha actual.');
+      return;
+    }
+
+    // Validar que todos los campos requeridos estén llenos
     if (!modelo || !color || !tipoAutomovil || !marcaAutomovil || !fecha || !placa || !imagen) {
       Alert.alert('Error', 'Todos los campos son requeridos.');
       return;
@@ -114,7 +153,7 @@ const AgregarVehiculo = ({ navigation }) => {
         type: 'image/jpeg',
         name: imagen.split('/').pop(),
       });
-      console.log('va con imagen')
+      console.log('va con imagen');
     }
 
     try {
@@ -126,7 +165,7 @@ const AgregarVehiculo = ({ navigation }) => {
       } else {
         Alert.alert('Error', response.error);
         console.log(response);
-        console.log(formData)
+        console.log(formData);
       }
     } catch (error) {
       console.error(error);
@@ -141,15 +180,31 @@ const AgregarVehiculo = ({ navigation }) => {
     }
   };
 
-  // Función para limitar la longitud de la placa a 6 dígitos
   const handleChangePlaca = (text) => {
-    if (text.length <= 6) {
-      setPlaca(text);
-    }
+    // Limpiar el valor de cualquier carácter que no sea letras, números o guiones
+    let cleanText = text.replace(/[^A-Z0-9-]/g, '').toUpperCase();
+
+    // Actualizar el estado de la placa
+    setPlaca(cleanText);
   };
 
+  const showPlacaInfo = () => {
+    Alert.alert('Información de Placa', 'Prefijos disponibles para placas salvadoreñas: A,AB,C,CC,CD,D,E,F,M,MB,MI,N,O,P,PR,PNC,RE,T,V.');
+  };
+
+
   return (
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.placaContainer}>
+        <TouchableOpacity onPress={showPlacaInfo} style={styles.infoIcon}>
+          <Text style={styles.imageButtonText}>Informacion de placa</Text>
+          <Image
+            source={require('../images/icons/iconInterrogacion.png')} // Asegúrate de que esta ruta sea correcta
+            style={styles.icon}
+          />
+        </TouchableOpacity>
+      </View>
+
       <Input
         placeholder="Modelo automóvil"
         value={modelo}
@@ -178,12 +233,14 @@ const AgregarVehiculo = ({ navigation }) => {
         value={fecha}
         onChangeText={handleChangeFecha} // Actualiza el estado de la fecha con validación
       />
+
       <Input
         placeholder="Placa automóvil"
         value={placa}
-        onChangeText={handleChangePlaca} // Actualiza el estado de la placa con validación
+        onChangeText={handleChangePlaca}
+        maxLength={11}
       />
-      <Button textoBoton='Guardar' accionBoton={handleGuardarCarro} />
+
       <TouchableOpacity style={styles.imageButton} onPress={handleAgregarImagen}>
         <Text style={styles.imageButtonText}>Agregar imagen</Text>
         <Text style={styles.imageButtonPlus}>+</Text>
@@ -197,17 +254,33 @@ const AgregarVehiculo = ({ navigation }) => {
         )}
       </View>
 
-    </View>
+      <Button textoBoton='Guardar' accionBoton={handleGuardarCarro} />
+
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     padding: 20,
     backgroundColor: 'white',
     alignItems: 'center',
     paddingBottom: 180,
+  },
+  placaContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+    width: '100%',
+  },
+  infoIcon: {
+    marginLeft: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: {
+    width: 24,
+    height: 24,
   },
   imageButton: {
     flexDirection: 'row',
@@ -217,6 +290,7 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     padding: 10,
     marginBottom: 12,
+    marginTop: 10,
     backgroundColor: 'white',
   },
   imageButtonText: {
@@ -242,3 +316,20 @@ const styles = StyleSheet.create({
 });
 
 export default AgregarVehiculo;
+
+const fetchMarcasAutomovil = async () => {
+  try {
+    const responseMarcasAutomoviles = await fetchData(API, 'readMarcas');
+    if (responseMarcasAutomoviles.status) {
+      setPickerValuesMarca(responseMarcasAutomoviles.dataset.map(item => ({
+        label: item.nombre_marca_automovil, // Asegúrate de que el campo nombre sea correcto
+        value: item.id_marca_automovil, // Asegúrate de que el campo id sea correcto
+      })));
+    } else {
+      Alert.alert('Error', `${responseMarcasAutomoviles.error}` + '. Es necesario registrar un automóvil antes de agendar una cita.');
+    }
+  } catch (error) {
+    console.error('Error en leer los elementos:', error);
+    Alert.alert('Error', 'Hubo un error.');
+  }
+};
